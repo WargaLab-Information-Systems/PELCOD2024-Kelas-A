1bd648751b918249359729e817124c052cf8e711
febrianti nur anggraini <fanggraini894@gmail.com>
2024-09-20 21:02:42 +0700

    Tugas pelatihan coding per 1

---
+++ tugas/240441100024_FEBRIANTI NUR ANGGRAINI/240441100024_FEBRIANTINURANGGRAINI.py
@@ -0,0 +1,30 @@
+# Definisi karakter
+nama1 = "Febby"
+nama2 = "Anisa"
+
+print(f"{nama1}: Hallo,Anisa udah lama kita gak ketemu")
+print(f"{nama2}: hallo Feb,iya soalnya rumah aku pindah di Mojoanyar")
+print(f"{nama1}: ohh pantesan,kamu sekarang sekolah dimana?")
+print(f"{nama2}: aku sekarang sekolah di SMAN 1 Mojokerto,kalo kamu sekolah dimana?")
+print(f"{nama1}: kalo aku sekolah di SMKN 2 Mojokerto ")
+print(f"{nama2}: sekarang aku sudah lama gak lihat kamu joging di gelora A.YANI")
+print(f"{nama1}: iya aku sekarang sering joging di gelora GAJAHMADA")
+print(f"{nama2}: kalo dilihat lihat badanmu sekarang tambah tinggi")
+tinggi_badan_febby = int(input(f"{nama1}: emang tinggi badan kamu sekarang berapa?")) 
+print(f"{nama2}: oh jadi sekarang tinggi badanmu {tinggi_badan_febby}cm.aku sekarang {tinggi_badan_febby + 5}cm")
+print(f"{nama1}: ternyata selisih tinngi badan kita cuma {5}cm ya?")
+selisih_tinggi_badan = (tinggi_badan_febby-(tinggi_badan_febby) + 5)
+print(f"{nama2}: iya,benar sekali,padahal dulu tinggi badan kita sama")
+print(f"{nama1}: kalo gitu bagaimana kalo kita mencoba hitung total tinggi badan kita?")
+total_tinggi_badan = tinggi_badan_febby + (tinggi_badan_febby + 5)
+print(f"{nama2}: total tinggi badan kita adalah {total_tinggi_badan}cm")
+print(f"{nama1}: wahh ternyata tinggi badan kita segitu ya kalo di total")
+print(f"{nama2}: iyaa,kalo gitu next time kita ayo joging bareng bareng kaya dulu lagi")
+print(f"{nama1}: wah ide bagus itu")
+print(f"{nama2}: baik lah kalo gitu see you next time")
+print(f"{nama1}: see you")
+
+
+
+
+
