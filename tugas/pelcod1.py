7c746fbaba4d918c297a9fcde59cb12028990e72
hilya-23 <hilyatulabidah34@gmail.com>
2024-09-20 15:32:54 +0700

    240441100132

---
+++ tugas/pelcod1.py
@@ -0,0 +1,30 @@
+nama_orang_1 = "Indah"
+nama_orang_2 = "gina"
+
+print("pada suatu hari di ruangan UTBK, ada dua orang yang kebetulan duduk bersama yaitu gina dan indah")
+print("dengan berani gina mengajak kenalan seseorang yang disampingnya")
+print("halo, nama kamu siapa?")
+print("oh haii, aku", nama_orang_1, "lalu kamu siapa?")
+print ("hai indah, aku", nama_orang_2, "salam kenal ya")
+print("salam kenal ya, ayo mutualan siapa tau nanti kita 1 kampus")
+print("oh iya, kamu mulai belajar utbk sejak kapan gina?")
+print("aku belajar mempersiapkan ini dari 7 bulan yang lalu, bagaimana denganmu indah?")
+print("hahaha aku baru 1 bulan lalu gin, soalnya habis ketolak di Snbp.")
+print("yaudah, see you ya semoga hasilnya memuaskan")
+
+print("setelah satu bulan selesai utbk, mereka pun mendapatkan pengumuman dan keduanya masuk di UTM")
+nilai_gina = int(input("nilai gina adalah :"))
+nilai_indah = int(input("nilai indah adalah :"))
+
+print("selisih nilai indah dan gina hanya sedikit namun gina masuk ke pilihan 1 sedangkan indah di pilihan dua")
+print("gina masuk di Psikologi dan indah masuk di majamen")
+
+nilai_aman_psikologi = 680
+nilai_aman_manajemen = 650
+Psikologi = 680 - nilai_gina
+Manajemen = 650 - nilai_indah
+
+print(f"pada saat tes, nilai indah yaitu {nilai_indah} kurang cukup untuk masuk ke Psikologi, total kekurangan nilai indah untuk masuk psikologi yaitu {Psikologi} sehingga indah bisa masuk di manajemen karena nilainya cukup karena manajemen membutuhkan setidaknya {nilai_aman_manajemen} untuk nilai amannya.")
+print(f"sedangkan nilai gina yaitu {nilai_gina} sangat cukup untuk masuk psikologi yang membutuhkan nilai {nilai_aman_psikologi}")
+
+print("setelah itu mereka tetap menjadi berteman baik meskipun berbeda prodi dan selalu mengerjakan tugas bersama.")
